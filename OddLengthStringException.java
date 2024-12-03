c50c06dd427d45bbe8c2235d2b383e7fe7f0cfd2
Kameron Ibraheem <kameronibraheem@gmail.com>
2024-12-02 19:32:19 -0600

    Create OddLengthStringException.java

---
+++ OddLengthStringException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kamer
+ */
+public class OddLengthStringException extends Exception {
+   public OddLengthStringException() {
+      super("Error: Odd length string. Cannot divide into equal halves.");
+   }
+}
+
